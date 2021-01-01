4dff449351db236e7286f61114c61c818ad7c013
Tashkyn Murat <tashkynmurat@gmail.com>
2021-01-02 05:12:18 +0600

    Finish generate message

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 const generateRandomNumber = num => Math.floor(Math.random() * num);
 
-const gameForRecommendation = {
-    signForGame: ['lucky', 'loser', 'bad', 'good', 'best']
-}
+const outputs = {
+    sign: ['cat', 'dog', 'tiger', 'elephant', 'lion'],
+    luckStatus: ['bad luck', 'worst luck', 'average luck', 'good luck', 'best luck'],
+    action: ['go eat', 'go to casino', 'stay home', 'go sleep']
+};
+
+let mixedMessage = [];
+
+for (let key in outputs) {
+    let numberOfIndex = generateRandomNumber(outputs[key].length);
+    
+    switch(key) {
+        case 'sign':
+            mixedMessage.push(`Your sign right now is '${outputs[key][numberOfIndex]}'`);
+            break;
+        case 'luckStatus':
+            mixedMessage.push(`You are having ${outputs[key][numberOfIndex]}`);
+            break;
+        case 'action':
+            mixedMessage.push(`You should ${outputs[key][numberOfIndex]}`);
+    };
+};
+
